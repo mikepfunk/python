4e5d80de74c2958e4799c339eed1dc3fce4e016b
MikePfunk28 <mike.pfundt@gmail.com>
2024-07-22 16:01:55 -0400

    added cryptid, using functions and classes, and experimenting with both.  In order to traverse a list, use range(len()).

--- cryptid.py
+++ cryptid.py
@@ -1,8 +1,11 @@
 # prompt user for name, then ask for a number to put after
 # each letter.
 
+# not defining cryptid here would result in error
+
 name = input("Enter your name: ")
 cryptid = ''
+# if I take out the below loop, and do not initialize number, I get an error.
 
 for i in name:
     number = input("Enter a number: ")
@@ -18,7 +21,16 @@ class CryptId:
         
     def display_info(self):
         print(f"Name: {self.name}\nNumber: {self.number}")
+        # This is how you print each element of a list, using the len function.
+        for i in range(len(name)):
+            print(name[i])
         
+    def counter(self):
+        count = 0
+        for i in self.name:
+            count += 1
+        return count
+            
     def main():
         name = input("Enter your name: ")
         for i in name:
@@ -27,7 +39,7 @@ class CryptId:
                 break
             name = name.replace(i, i + number)
         print(name)
-    
+        
     if __name__ == '__main__':
         main()
         
@@ -36,6 +48,5 @@ if __name__ == '__main__':
     # Creating an instance of CryptId
     my_cryptid = CryptId(name, number)
     # Calling a method of the instance
-    my_cryptid.display_info()
-    
-    print(cryptid)
+    my_cryptid.display_info() # this is calling the display_info method
+    print(my_cryptid.counter()) # this will print how many iterations the loop did.
---
+++ structures.py
@@ -0,0 +1,6 @@
+names = ["Bob, Alice, Charlie"]
+
+for name in names:
+    list(names)
+    print(f"Hello, {name}")
+    print(list(names))
